08ca5162d9a42abf664137d1ee8ec6fa4ad7e219
Abu Bakr Khabibullayev <abubakr3006@gmail.com>
2022-02-03 15:46:06 +0500

    commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let elBtn = document.getElementById('btn');
+
+elBtn.addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+})
+
+
+
+
+
+
+// let elOpen = document.getElementById('open')
+// let elModal = document.getElementById('modal')
+// let elClose = document.getElementById('close')
+
+// elOpen.addEventListener('click', function() {
+//     elModal.classList.add('show')
+// })
+
+// elClose.addEventListener('click', function() {
+//     elModal.classList.remove('show')
+// })
